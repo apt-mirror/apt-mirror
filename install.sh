0665f594e253ac3a4ff2f9dcdc5b6990b3c6774b
Brandon Holtsclaw <me@brandonholtsclaw.com>
2013-03-03 01:20:33 -0600

    added install.sh and mirror.list cleanup

---
+++ install.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+
+BASE_PATH="/var/spool/apt-mirror"
+
+echo "Installing apt-mirror command ...";
+sudo cp apt-mirror /usr/local/bin/;
+sudo chmod +x /usr/local/bin/apt-mirror;
+
+echo "Installing apt-mirror configuration ...";
+sudo mkdir -p /etc/apt/;
+sudo cp mirror.list /etc/apt/;
+
+echo "Installing apt-mirror manual ...";
+sudo mkdir -p /usr/share/man/man1/
+sudo pod2man apt-mirror > /usr/share/man/man1/apt-mirror.1;
+
+echo "Creating base mirror structure ...";
+sudo mkdir -p ${BASE_PATH}
+sudo mkdir -p ${BASE_PATH}/mirror
+sudo mkdir -p ${BASE_PATH}/skel
+sudo mkdir -p ${BASE_PATH}/var
+
+echo "Installing postmirror script example ...";
+sudo cp postmirror.sh ${BASE_PATH}/var/
+
